171115c38bc139c09dfcc0055b2af35c5a14f450
Emil Grunt <emil.grunt@piscada.com>
2025-12-15 21:54:44 +0100

    Day 2: Adds solution for part 2 and cleans up solution

--- day2/main.cpp
+++ day2/main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <string_view>
 #include <vector>
 #include <cstdint>
 
@@ -13,8 +12,8 @@ struct ProductIdRange
     int64_t stop;
 };
 
-int64_t part1_string_based(const std::vector<ProductIdRange> &ranges);
-int64_t part1_number_based(const std::vector<ProductIdRange> &ranges);
+int64_t part1(const std::vector<ProductIdRange> &ranges);
+int64_t part2(const std::vector<ProductIdRange> &ranges);
 
 int main(int argc, char *argv[])
 {
@@ -34,51 +33,21 @@ int main(int argc, char *argv[])
         ranges.push_back(range);
     }
 
-    int64_t sum = part1_string_based(ranges);
+    int64_t sum = part2(ranges);
 
     std::cout << "sum = " << sum << std::endl;
 
     return 0;
 }
 
-int64_t part1_string_based(const std::vector<ProductIdRange> &ranges)
+int64_t part1(const std::vector<ProductIdRange> &ranges)
 {
     int64_t sum = 0;
     for (const auto &range : ranges)
     {
+#ifdef AOC_DEBUG
         std::cout << "Checking range: " << range.start << "-" << range.stop << std::endl;
-
-        for (int64_t i = range.start; i <= range.stop; i++)
-        {
-            std::string n = std::to_string(i);
-            std::string_view nv(n);
-            int length = n.size();
-
-            if (length % 2 != 0)
-            {
-                continue;
-            }
-
-            int half_length = length / 2;
-            std::string_view first = nv.substr(0, half_length);
-            std::string_view second = nv.substr(half_length, half_length);
-
-            if (first == second)
-            {
-                sum += i;
-            }
-        }
-    }
-
-    return sum;
-}
-
-int64_t part1_number_based(const std::vector<ProductIdRange> &ranges)
-{
-    int64_t sum = 0;
-    for (const auto &range : ranges)
-    {
-        std::cout << "Checking range: " << range.start << "-" << range.stop << std::endl;
+#endif
 
         for (int64_t i = range.start; i <= range.stop; i++)
         {
@@ -104,30 +73,87 @@ int64_t part1_number_based(const std::vector<ProductIdRange> &ranges)
     return sum;
 }
 
-int64_t part2_number_based(const std::vector<ProductIdRange> &ranges)
+bool compare(int64_t id, int parts)
 {
+    int exp = std::log10(id);
+
+    int64_t e = (exp / parts) + 1;
+    int64_t l = std::pow(10, e);
+
+    int64_t a = (id % l);
+    int64_t i = id;
+
+    for (int x = 1; x < parts; x++)
+    {
+        i /= l;
+
+        int64_t b = (i % l);
+        if (a != b)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int64_t part2(const std::vector<ProductIdRange> &ranges)
+{
+    static std::vector<int64_t> primes = { 19, 17, 13, 11, 7, 5, 3, 2 };
+
     int64_t sum = 0;
     for (const auto &range : ranges)
     {
+#ifdef AOC_DEBUG
         std::cout << "Checking range: " << range.start << "-" << range.stop << std::endl;
+#endif
 
-        for (int64_t i = range.start; i <= range.stop; i++)
+        for (int64_t id = range.start; id <= range.stop; id++)
         {
-            int64_t exp = std::log10(i);
-            if (exp % 2 == 0)
+            int exp = std::log10(id);
+            int digits = exp + 1;
+            int length = digits;
+
+            if (length == 1)
             {
                 continue;
             }
 
-            int64_t e = (exp / 2) + 1;
-            int64_t l = std::pow(10, e);
-
-            int64_t low = i % l;
-            int64_t high = i / l;
-
-            if (low == high)
+            std::vector<int> lengths = { digits };
+            for (int64_t prime : primes)
             {
-                sum += i;
+                if (length == 1)
+                {
+                    break;
+                }
+
+                int64_t multiplier = 1;
+                while (length % prime == 0)
+                {
+                    int l = prime * multiplier;
+                    if (l == digits)
+                    {
+                        break;
+                    }
+
+                    lengths.push_back(prime * multiplier);
+
+                    length /= prime;
+
+                    multiplier += 1;
+                }
+            }
+
+            for (int length : lengths)
+            {
+                if (compare(id, length))
+                {
+#ifdef AOC_DEBUG
+                    std::cout << id << " is invalid" << std::endl;
+#endif
+                    sum += id;
+                    break;
+                }
             }
         }
     }
