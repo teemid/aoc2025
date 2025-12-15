fd47cc4732108a6e884ad11a997ceef88efd4c4f
Emil Grunt <emil.grunt@piscada.com>
2025-12-15 23:07:01 +0100

    Day 5: Adds solution for part 2

--- day5/main.cpp
+++ day5/main.cpp
@@ -11,9 +11,11 @@ struct Range
 {
     int64_t start;
     int64_t stop;
+    bool reduced;
 };
 
 int64_t part1(const std::vector<int64_t> &available, const std::vector<Range> &fresh);
+int64_t part2(const std::vector<Range> &fresh);
 
 int main(int argc, char *argv[])
 {
@@ -46,6 +48,7 @@ int main(int argc, char *argv[])
         else
         {
             Range range;
+            range.reduced = false;
             char c;
 
             iss >> range.start;
@@ -53,7 +56,7 @@ int main(int argc, char *argv[])
             iss >> range.stop;
 
             assert(c == '-');
-    
+
             fresh_ingredients.push_back(range);
         }
     }
@@ -72,7 +75,7 @@ int main(int argc, char *argv[])
     }
 #endif
 
-    int64_t count = part1(available_ingredients, fresh_ingredients);
+    int64_t count = part2(fresh_ingredients);
 
     std::cout << count << " of the available ingredient IDs are fresh" << std::endl;
 
@@ -96,7 +99,6 @@ int64_t part1(const std::vector<int64_t> &available, const std::vector<Range> &f
 
 bool search(const std::vector<Range> &fresh_ingredients, int64_t ingredient)
 {
-
     for (const Range &range : fresh_ingredients)
     {
         if (range.start <= ingredient && ingredient <= range.stop)
@@ -108,3 +110,79 @@ bool search(const std::vector<Range> &fresh_ingredients, int64_t ingredient)
     return false;
 }
 
+bool is_overlap(const Range &a, const Range &b)
+{
+    if (a.stop < b.start)
+    {
+        return false;
+    }
+    else if (b.stop < a.start)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+int reduce_ranges(std::vector<Range> &ranges)
+{
+    int reduced_ranges = 0;
+    for (int i = 0; i < ranges.size(); i++)
+    {
+        Range &a = ranges[i];
+        for (int j = 0; j < ranges.size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            const Range &b = ranges[j];
+            if (!is_overlap(a, b))
+            {
+                continue;
+            }
+            else
+            {
+                a.start = std::min(a.start, b.start);
+                a.stop = std::max(a.stop, b.stop);
+
+                reduced_ranges += 1;
+
+                ranges.erase(ranges.begin() + j);
+
+                i = std::max(i - 1, 0);
+                j = std::max(j - 1, 0);
+            }
+        }
+    }
+
+    return reduced_ranges;
+}
+
+int64_t part2(const std::vector<Range> &fresh)
+{
+    int64_t count = 0;
+
+    std::vector<Range> unique_ranges;
+    std::vector<Range> ranges = fresh;
+
+    for ( ; ; )
+    {
+        int reduced_ranges = reduce_ranges(ranges);
+#ifdef AOC_DEBUG
+        std::cout << "Reduced " << reduced_ranges << " number of ranges" << std::endl;
+#endif 
+        if (reduced_ranges == 0)
+        {
+            break;
+        }
+    }
+
+    for (const Range &range : ranges)
+    {
+        count += (range.stop - range.start) + 1;
+    }
+
+    return count;
+}
